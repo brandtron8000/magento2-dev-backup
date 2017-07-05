1f662bbaf7fb3a88ec998913a4be76b1ab80ef93
admin <admin@admins-MacBook-Pro.local>
2017-07-05 12:47:55 -0700

    Git Clone Initial Commit

---
+++ app/code/Magento/Ui/Component/Filters/Type/Date.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Ui\Component\Filters\Type;
+
+use Magento\Ui\Component\Form\Element\DataType\Date as DataTypeDate;
+
+/**
+ * @api
+ */
+class Date extends AbstractFilter
+{
+    const NAME = 'filter_date';
+
+    const COMPONENT = 'date';
+
+    /**
+     * Wrapped component
+     *
+     * @var DataTypeDate
+     */
+    protected $wrappedComponent;
+
+    /**
+     * Date format
+     *
+     * @var string
+     */
+    protected static $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Prepare component configuration
+     *
+     * @return void
+     */
+    public function prepare()
+    {
+        $this->wrappedComponent = $this->uiComponentFactory->create(
+            $this->getName(),
+            static::COMPONENT,
+            ['context' => $this->getContext()]
+        );
+        $this->wrappedComponent->prepare();
+        // Merge JS configuration with wrapped component configuration
+        $jsConfig = array_replace_recursive(
+            $this->getJsConfig($this->wrappedComponent),
+            $this->getJsConfig($this)
+        );
+        $this->setData('js_config', $jsConfig);
+
+        $this->setData(
+            'config',
+            array_replace_recursive(
+                (array)$this->wrappedComponent->getData('config'),
+                (array)$this->getData('config')
+            )
+        );
+
+        $this->applyFilter();
+
+        parent::prepare();
+    }
+
+    /**
+     * Apply filter
+     *
+     * @return void
+     */
+    protected function applyFilter()
+    {
+        if (isset($this->filterData[$this->getName()])) {
+            $value = $this->filterData[$this->getName()];
+
+            if (empty($value)) {
+                return;
+            }
+
+            if (is_array($value)) {
+                $dateFrom = null;
+                $dateTo = null;
+
+                if (isset($value['from'])) {
+                    $dateFrom = $this->wrappedComponent->convertDate($value['from']);
+                    if ($this->getData('config/skipTime')) {
+                        $dateFrom->setTime(0, 0, 0);
+                    }
+                    $this->applyFilterByType('gteq', $dateFrom);
+                }
+
+                if (isset($value['to'])) {
+                    $dateTo = $this->wrappedComponent->convertDate($value['to']);
+                    if ($this->getData('config/skipTime')) {
+                        $dateTo->setTime(0, 0, 0);
+                    }
+                    if ($dateFrom == $dateTo) {
+                        $dateTo->setTime(23, 59, 59);
+                    }
+                    $this->applyFilterByType('lteq', $dateTo);
+                }
+            } else {
+                $this->applyFilterByType('eq', $this->wrappedComponent->convertDate($value));
+            }
+        }
+    }
+
+    /**
+     * Apply filter by its type
+     *
+     * @param string $type
+     * @param string $value
+     * @return void
+     */
+    protected function applyFilterByType($type, $value)
+    {
+        if (!empty($value)) {
+            $filter = $this->filterBuilder->setConditionType($type)
+                ->setField($this->getName())
+                ->setValue($value->format(static::$dateFormat))
+                ->create();
+
+            $this->getContext()->getDataProvider()->addFilter($filter);
+        }
+    }
+}
